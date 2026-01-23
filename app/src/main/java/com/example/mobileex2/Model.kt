091988003c09553b85924585fcb4a5703df1c98b
Ben <benlevi8010@gmail.com>
2026-01-23 16:16:04 +0200

    added students rows

---
+++ app/src/main/java/com/example/mobileex2/Model.kt
@@ -0,0 +1,12 @@
+package com.example.mobileex2
+
+object Model {
+    val data = mutableListOf<Student>()
+
+    init {
+        // Add some dummy data so the list isn't empty
+        for (i in 0..10) {
+            data.add(Student("Student $i", "$i", "054-000000$i", "Address $i", false))
+        }
+    }
+}
